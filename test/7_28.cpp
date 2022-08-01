c907547656d6e304ff634c541bfdcb0dc54c6f40
HCXXYBBD <103500453+HCXXYBBD@users.noreply.github.com>
2022-08-01 23:23:53 +0800

    Add files via upload

---
+++ test/7_28.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"你好Welcome \nto \nVScode!!!";
+    char name[40]={0};
+    cout<<"请输入你的名字:>";
+    cin>>name;
+    cout<<"你的名字是:>"<<name;
+}
